e22c155960e8d0c2fe84ff1ffd6bc948bd23a145
aku <aku_88@hotmail.com>
2018-04-02 22:09:26 +0300

    Changing commit list

--- pdf_generator.py
+++ pdf_generator.py
@@ -13,22 +13,20 @@ class MakePDF:
     def make_report(self, input_list, input_header):
 
         # Check if output-directory exists. If not, create one
-        if not os.path.exists("output"):
-            os.makedirs("output")
+        if not os.path.exists("reports"):
+            os.makedirs("reports")
         """ A function to make a pdf. Can be inputted eg a list returned from gimme_my_todo_list()"""
 
         # Define pdf-name with path to save output to.
-#       pdf_name = "output_" + datetime.datetime.now().strftime("%d-%m-%Y_%H-%M-%S") + ".pdf"
-        pdf_name = "output-" + "TEST" + ".pdf"
-
-        save_name = os.path.join("output/", pdf_name)
+        pdf_name = "Report_" + datetime.datetime.now().strftime("%d-%m-%Y_%H-%M-%S") + ".pdf"
+        save_name = os.path.join("reports/", pdf_name)
 
         # Create canvas
         self.c = canvas.Canvas(save_name)
 
         # Write header to canvas
         self.c.setFont("Helvetica-Bold", 17, leading=None)
-        self.c.drawString(30, self.line, input_header + " " + datetime.datetime.now().strftime("%d-%m-%Y @ %H:%M"))
+        self.c.drawString(30, self.line, input_header + " " + datetime.datetime.now().strftime("%d.%m.%Y @ %H:%M"))
         self.line -= 30
 
         # Roll trough the list and write jobs to canvas with _generate_job_info function.
@@ -62,22 +60,23 @@ class MakePDF:
 
         # Continue on the next page if there is too little space left in the current page.
         if self.line < 130:
-            self.c.drawString(130, self.line - 15, "More jobs on the next page --->")
+            self.c.drawString(30, self.line - 15, "More jobs on the next page >>")
             self.line = 790
             self.c.showPage()
 
         #Draw a fucking rectangle
-        color1 = HexColor("#DDDDDD")
+        color1 = HexColor("#DDDDCC")
         self.c.setFillColor(color1)
         self.c.rect(25,self.line - 2,550,12, fill=True, stroke=False) 
 
-        # Write details of job on canvas
+        # Write details of job on canvas in rectangle
         self.c.setFillColor(black)
         self.c.setFont("Helvetica-Bold", 11, leading=None)
         self.c.drawString(30, self.line, customer + " : " + product + " : " + addedDate)
+        self.c.drawString(500, self.line, "PR {} : ID {}".format(priority, job_id))
+        self.line -= 14
 
-        self.c.drawString(510, self.line, "PR: {}, ID: {}".format(priority, job_id))
-        self.line -= 16
+        # Writing rest of the info under rectangle
         self.c.setFont("Helvetica", 11, leading=None)
         self.c.drawString(30, self.line, "Amount: ")
         self.c.drawString(96, self.line, amount)
